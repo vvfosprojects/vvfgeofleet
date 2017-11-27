3fd6d5600e2138b0f9217e88559723e96953661c
supix <esposito.marce@gmail.com>
2017-11-27 21:46:38 +0100

    Camel case per il formato Json
    Il formato Json dei pacchetti restituiti rispetta il formato Camel Case
    invece del Upper Camel Case (aka Pascal Case).

--- src/VVFGeoFleet/App_Start/WebApiConfig.cs
+++ src/VVFGeoFleet/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -19,6 +20,9 @@ namespace VVFGeoFleet
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
         }
     }
 }
