5bf576f1604d7b808b5bf8a957423024b58df705
supix <esposito.marce@gmail.com>
2017-12-02 22:27:51 +0100

    log4net integrato
    E' stata aggiunta la dipendenza dalla libreria log4net per il logging
    applicativo.
    
    Ogni comando inviato dal driver al database MongoDB è loggato nell'area
    trace dell'ambiente di sviluppo. E' predisposto il logging su file con
    rotazione con apposito appender presente nel web.config.

--- src/VVFGeoFleet/Global.asax.cs
+++ src/VVFGeoFleet/Global.asax.cs
@@ -26,6 +26,7 @@ namespace VVFGeoFleet
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
