a6093cbe98227cabe93d446fc068c575f54abc77
felix448 <felice.deflorio@email.it>
2018-02-22 19:03:44 +0100

    Dashboard con polling
    E' presente la funzione di aggiornamento continuo delle posizioni dei
    Mezzi.
    Da ottimizzare la visualizzazione su Mappa con le sole variazioni.

--- src/frontend/dashboard/src/app/app.component.ts
+++ src/frontend/dashboard/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PosizioneMezzo } from './posizione-mezzo/posizione-mezzo.model';
 import { PosizioneFlottaService } from './service-VVFGeoFleet/posizione-flotta.service';
 import { PosizioneFlottaServiceFake } from './service-VVFGeoFleet/posizione-flotta.service.fake';
+import { Observable } from "rxjs/Rx";
 
 
 @Component({
@@ -17,10 +18,12 @@ export class AppComponent {
 
   private elencoPosizioniMezzo : PosizioneMezzo[] = [];
 
-
+  private timer;
+  private timerSubcribe: PushSubscription;
         constructor(private posizioneFlottaService: PosizioneFlottaService) { 
         }
         
+        /*
         ngOnInit() { this.posizioneFlottaService.getPosizioneFlotta()
           .subscribe( posizioni => {
             console.log("posizioneFlottaService: ", posizioni);
@@ -35,6 +38,60 @@ export class AppComponent {
 
       
         }   
+        */
+        /*
+        ngOnInit() { 
+       
+          var observable = Observable.create((observer) => {
+            var id = setInterval(() => {
+              observer.next(console.log('observer.next su setInterval()'));
+            }, 2000);
+    
+            setTimeout(() => {
+              //clearInterval(id);
+              //observer.complete();
+              observer.next(console.log('observer.next su setTimeout()'));     
+            }, 5000);        
+          }); 
 
+             
+          observable = this.posizioneFlottaService.getPosizioneFlotta()
+          .subscribe( posizioni => {
+            console.log("posizioneFlottaService: ", posizioni);
+            this.elencoPosizioniMezzo = posizioni.sort( 
+              function(a,b) 
+              { var bb : Date = new Date(b.istanteAcquisizione);
+                var aa : Date  = new Date(a.istanteAcquisizione);
+                return aa>bb ? -1 : aa<bb ? 1 : 0;
+              }
+            );
+          });
+        }   
+      */        
+       ngOnInit() { 
+          this.timer = Observable.timer(2000,5000).timeout(10000);
+          this.timerSubcribe = this.timer.subscribe(t => this.aggiorna(t));
+        }   
+
+        aggiorna(tt) {
+          this.posizioneFlottaService.getPosizioneFlotta()
+          .subscribe( posizioni => {
+            //console.log("posizioneFlottaService: ", posizioni);
+            this.elencoPosizioniMezzo = posizioni.sort( 
+              function(a,b) 
+              { var bb : Date = new Date(b.istanteAcquisizione);
+                var aa : Date  = new Date(a.istanteAcquisizione);
+                return aa>bb ? -1 : aa<bb ? 1 : 0;
+              }
+            );
+          });
+            
+        }
+    
+        ngOnDestroy(){
+          this.timerSubcribe.unsubscribe();
+          console.log("Destroy timer");
+      
+        }
 
 }
