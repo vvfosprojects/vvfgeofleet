69e27004a76cf4f6e596e2f78afa090c83049a99
supix <esposito.marce@gmail.com>
2018-03-07 16:32:31 +0100

    Metodo API per il calcolo del percorso di un mezzo
    Esiste un metodo API utile a conoscere il percorso di un mezzo. Il
    metodo è il seguente:
    
    GET /api/percorso/{codiceMezzo}?from=2018-01-01T00:00:00Z&to=2018-03-04T10:45:52.875Z
    
    Dato un codice mezzo ed un intervallo temporale, vengono restituiti
    tutti i messaggi di posizione relativi al mezzo nell'intervallo
    temporale, con la conseguente possibilità di costruirne il percorso su
    mappa.
    
    La query è indicizzata.

---
+++ src/backend/Persistence.MongoDB/Servizi/GetPercorso_DB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modello.Classi;
+using Modello.Servizi.Persistence;
+using MongoDB.Driver;
+
+namespace Persistence.MongoDB.Servizi
+{
+    internal class GetPercorso_DB : IGetPercorso
+    {
+        private readonly IMongoCollection<MessaggioPosizione> messaggiPosizioneCollection;
+
+        public GetPercorso_DB(IMongoCollection<MessaggioPosizione> messaggiPosizioneCollection)
+        {
+            this.messaggiPosizioneCollection = messaggiPosizioneCollection ?? throw new ArgumentNullException(nameof(messaggiPosizioneCollection));
+        }
+
+        public IEnumerable<MessaggioPosizione> Get(string codiceMezzo, DateTime from, DateTime to)
+        {
+            return this.messaggiPosizioneCollection.Find(m =>
+                m.CodiceMezzo == codiceMezzo &&
+                m.IstanteAcquisizione >= from &&
+                m.IstanteAcquisizione <= to)
+                .SortBy(m => m.IstanteAcquisizione)
+                .ToEnumerable();
+        }
+    }
+}
