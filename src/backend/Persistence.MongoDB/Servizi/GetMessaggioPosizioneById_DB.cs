4518c24017cefa5fc4085d1340cafcde4f465832
supix <esposito.marce@gmail.com>
2018-03-16 16:41:20 +0100

    CodeMaid executed on whole project.

--- src/backend/Persistence.MongoDB/Servizi/GetMessaggioPosizioneById_DB.cs
+++ src/backend/Persistence.MongoDB/Servizi/GetMessaggioPosizioneById_DB.cs
@@ -18,10 +18,7 @@
 // </copyright>
 //-----------------------------------------------------------------------
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Modello.Classi;
 using Modello.Servizi.Persistence;
 using MongoDB.Driver;
