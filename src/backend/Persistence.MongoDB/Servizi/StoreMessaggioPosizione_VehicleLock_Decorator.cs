58d87367c54982ced055b623ebf9f631411fba6e
supix <esposito.marce@gmail.com>
2018-04-20 21:46:05 +0200

    Lock on vehicleCode upon position message insertion
    Upon position message insertion, a lock is acquired on vehicleCode.
    Thus, two concurrent thread simultaneously inserting on the same vehicle
    (which is unlikely but not impossible) cannot cause a race condition.
    
    This update requires web.config to be extended with this two new
    parameters:
    
    <!-- vehicle lock on insertion parameters - start -->
    
        <!-- number of attempts to acquire the lock, after which the insertion gives up. -->
        <add key="numberOfRetries" value="5" />
        <!-- interval between attempts to acquire lock -->
        <add key="retriesInterval_msec" value="5000" />
    
    <!-- vehicle lock on insertion parameters - end -->
    
    Locking is managed through a new collection called vehicleLocks. This
    collection is almost always empty, except during an insertion. The
    inserting thread atomically create a document within this collection,
    having the _id field equal to the vehicle code to be inserted. One more
    such record cannot be isimultaneously inserted by another thread for the
    same vehicle since this would violate the _id unicity.
    
    vehicleLocks collection has an expiring index on the ts (insertion time)
    field. This prevents an everlasting lock in case of application failure
    during an insertion (see
    https://docs.mongodb.com/manual/tutorial/expire-data/).

---
+++ src/backend/Persistence.MongoDB/Servizi/StoreMessaggioPosizione_VehicleLock_Decorator.cs
@@ -0,0 +1,106 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StoreMessaggioPosizione_VehicleLock_Decorator.cs" company="CNVVF">
+// Copyright (C) 2017 - CNVVF
+//
+// This file is part of VVFGeoFleet.
+// VVFGeoFleet is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// SOVVF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using Modello.Classi;
+using Modello.Servizi.Persistence;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Persistence.MongoDB.Classes;
+
+namespace Persistence.MongoDB.Servizi
+{
+    /// <summary>
+    ///   Performs a pessimistic lock on vehicleCode on position message insertion. This prevents
+    ///   race conditions on simultaneous insertion of position messages for the same vehicle (which
+    ///   is unlike but possible).
+    /// </summary>
+    internal class StoreMessaggioPosizione_VehicleLock_Decorator : IStoreMessaggioPosizione
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly IStoreMessaggioPosizione decorated;
+        private readonly IMongoCollection<VehicleLock> vehicleLocksCollection;
+
+        public StoreMessaggioPosizione_VehicleLock_Decorator(
+            IStoreMessaggioPosizione decorated,
+            IMongoCollection<VehicleLock> vehicleLocksCollection)
+        {
+            this.decorated = decorated ?? throw new ArgumentNullException(nameof(decorated));
+            this.vehicleLocksCollection = vehicleLocksCollection ?? throw new ArgumentNullException(nameof(vehicleLocksCollection));
+        }
+
+        /// <summary>
+        ///   Number of attempts to acquire the lock, after which the insertion gives up.
+        /// </summary>
+        public int NumberOfRetries { get; set; }
+
+        /// <summary>
+        ///   Interval between attempts to acquire lock
+        /// </summary>
+        public int RetriesInterval_msec { get; set; }
+
+        public void Store(MessaggioPosizione newMessage)
+        {
+            var vehicleCode = newMessage.CodiceMezzo;
+
+            // acquire the lock
+            var lockDoc = new VehicleLock(vehicleCode);
+
+            var lockAcquired = false;
+            var attempts = 0;
+
+            while (!lockAcquired && attempts < this.NumberOfRetries)
+            {
+                try
+                {
+                    this.vehicleLocksCollection.InsertOne(lockDoc);
+                    lockAcquired = true;
+                }
+                catch
+                {
+                    attempts++;
+
+                    if (attempts < this.NumberOfRetries)
+                    {
+                        log.Warn($"Cannot acquire lock for vehicle { vehicleCode }. Attempt #{ attempts } of { this.NumberOfRetries }.");
+                    }
+                    else
+                    {
+                        log.Error($"Cannot acquire lock for vehicle { vehicleCode }. Giving up on insertion after { this.NumberOfRetries } attempts.");
+                        throw;
+                    }
+
+                    Thread.Sleep(this.RetriesInterval_msec);
+                }
+            }
+
+            // inserts the positione message
+            this.decorated.Store(newMessage);
+
+            // release the lock
+            this.vehicleLocksCollection.DeleteOne(doc => doc.VehicleCode == vehicleCode);
+        }
+    }
+}
