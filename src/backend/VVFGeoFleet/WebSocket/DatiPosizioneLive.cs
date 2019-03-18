027aa9685badcd45b976f15f8bb04390f112a0e3
clod28 <37631289+clod28@users.noreply.github.com>
2019-03-18 09:30:16 +0100

    Normalizzata la serializzazione in JSON
    Aggiunta Strategy Camel Case Naming nella serializzazione JSON
    Modificato url socket da "/PosizioneLive" a "/api/PosizioneLive"

--- src/backend/VVFGeoFleet/WebSocket/DatiPosizioneLive.cs
+++ src/backend/VVFGeoFleet/WebSocket/DatiPosizioneLive.cs
@@ -1,35 +1,19 @@
- 
-﻿//-----------------------------------------------------------------------
-// <copyright file="DatiPosizioneLive.cs" company="CNVVF">
-// Copyright (C) 2017 - CNVVF
-//
-// This file is part of VVFGeoFleet.
-// VVFGeoFleet is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// SOVVF is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// </copyright>
-//-----------------------------------------------------------------------
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web
+using System.Web;
 using Microsoft.AspNet.SignalR;
 using Modello.Classi;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace VVFGeoFleet
 {
+
     public class DatiPosizioneLive : PersistentConnection
     {
+
         protected override Task OnConnected(IRequest request, string connectionId)
         {
             return Connection.Send(connectionId, "Connesso!");
@@ -45,12 +29,17 @@ namespace VVFGeoFleet
             try
             {
                 GlobalHost.ConnectionManager.GetConnectionContext<DatiPosizioneLive>().Connection.Broadcast(
-                      Newtonsoft.Json.JsonConvert.SerializeObject(messaggio, Newtonsoft.Json.Formatting.None, new Newtonsoft.Json.JsonSerializerSettings { NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore })
-                     );
+                    JsonConvert.SerializeObject(messaggio, Newtonsoft.Json.Formatting.None, new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore
+                        ,
+                        ContractResolver = new DefaultContractResolver() { NamingStrategy = new CamelCaseNamingStrategy() }
+                    })
+
+                    );
             }
             catch
             {
-
 
             }
 
@@ -58,4 +47,3 @@ namespace VVFGeoFleet
         }
     }
 }
-
