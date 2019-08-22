vvfosprojects/vvfgeofleet

commit d6e67bc808d5b232a8fed83240a7bddd79766e55

Connection string no longer parsed
Custom connection string parsing did not work and was not cluster compliant. In order to let MongoDB parse the connection string, and in order to enable mongodb+srv//: connection strings formats, the connection string is no longer parsed and the client is directly created from the connection string contained within the Web.Config.

WARNING: due to this change, mongodb queries are no longer logged. A fix is needed for this.

Apart from this major change, Owin and compiler libraries have been updated.

src/backend/Persistence.MongoDB/DbContext.cs
@@ -39,7 +39,7 @@ namespace Persistence.MongoDB
         private static IMongoDatabase database;
         private static string[] nonLoggedCommandNames = new[] { "isMaster", "buildInfo" };
 
-        internal DbContext(string connectionString)
+        internal DbContext(string connectionString, string databaseName)
         {
             if (database == null)
             {
@@ -49,29 +49,8 @@ namespace Persistence.MongoDB
 
                 this.MapClasses();
 
-                var url = MongoUrl.Create(connectionString);
-                IEnumerable<MongoCredential> credentials;
-                if (!string.IsNullOrWhiteSpace(url.Username))
-                    credentials = new[] { MongoCredential.CreateCredential(url.DatabaseName, url.Username, url.Password) };
-                else
-                    credentials = Enumerable.Empty<MongoCredential>();
-
-                var settings = new MongoClientSettings
-                {
-                    ClusterConfigurator = cb =>
-                    {
-                        cb.Subscribe<CommandStartedEvent>(e =>
-                        {
-                            if (!nonLoggedCommandNames.Contains(e.CommandName))
-                                log.Debug($"{e.CommandName} - {e.Command.ToJson()}");
-                        });
-                    },
-                    Server = url.Server,
-                    Credentials = credentials
-                };
-
-                var client = new MongoClient(settings);
-                database = client.GetDatabase(url.DatabaseName);
+                var client = new MongoClient(connectionString);
+                database = client.GetDatabase(databaseName);
 
                 this.CreateIndexes();
             }

src/backend/Modello/Configurazione/IAppConfig.cs
@@ -23,6 +23,7 @@ namespace Modello.Configurazione
     public interface IAppConfig
     {
         string ConnectionString { get; }
+        string DatabaseName { get; }
         int OrizzonteTemporale_sec { get; }
 
         #region Interpolation data

src/backend/Persistence.MongoDB/Bindings.cs
@@ -30,7 +30,8 @@ namespace Persistence.MongoDB
         {
             container.Register<DbContext>(() =>
             {
-                return new DbContext(container.GetInstance<Modello.Configurazione.IAppConfig>().ConnectionString);
+                return new DbContext(container.GetInstance<Modello.Configurazione.IAppConfig>().ConnectionString,
+                    container.GetInstance<Modello.Configurazione.IAppConfig>().DatabaseName);
             }, Lifestyle.Singleton);
 
             container.Register<global::MongoDB.Driver.IMongoCollection<Modello.Classi.MessaggioPosizione>>(() =>

src/backend/VVFGeoFleet.IntegrationTest/PositionMessageStorage.cs
@@ -37,7 +37,7 @@ namespace VVFGeoFleet.IntegrationTest
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
-            dbContext = new DbContext("mongodb://localhost:27017/" + dbName);
+            dbContext = new DbContext("mongodb://localhost:27017/" + dbName, dbName);
         }
 
         [OneTimeTearDown]

src/backend/VVFGeoFleet.Test/CreazioneDatiFake.cs
@@ -36,7 +36,7 @@ namespace VVFGeoFleet.Test
         [Ignore("It's not a test fixture. It is intended to fill database with fake data.")]
         public void Popola_database_con_dati_fake()
         {
-            var dbContext = new DbContext("mongodb://localhost:27017/vvfgeofleet");
+            var dbContext = new DbContext("mongodb://localhost:27017/vvfgeofleet", "vvfgeofleet");
             var fakerFonte = new Faker<Fonte>()
                 .StrictMode(true)
                 .RuleFor(l => l.CodiceFonte, f => f.Random.Replace("?##"))

src/backend/VVFGeoFleet/Configurazione/AppConfig_FromWebConfig.cs
@@ -28,6 +28,7 @@ namespace VVFGeoFleet.Configurazione
     public class AppConfig_FromWebConfig : IAppConfig
     {
         public readonly string connectionString = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+        public readonly string databaseName = Convert.ToString(ConfigurationManager.AppSettings["dbname"]);
         public readonly int orizzonteTemporale_sec = Convert.ToInt32(ConfigurationManager.AppSettings["orizzonteTemporale_sec"]);
         public readonly bool interpolationActive = Convert.ToBoolean(ConfigurationManager.AppSettings["interpolationActive"]);
         public readonly float interpolationThreshold_mt = Convert.ToSingle(ConfigurationManager.AppSettings["interpolationThreshold_mt"]);
@@ -39,6 +40,7 @@ namespace VVFGeoFleet.Configurazione
         public readonly string[] authorizedIpSources = Convert.ToString(ConfigurationManager.AppSettings["authorizedIpSources"]).Split(',').Select(s => s.Trim()).ToArray();
 
         public string ConnectionString { get => this.connectionString; }
+        public string DatabaseName { get => this.databaseName; }
         public int OrizzonteTemporale_sec { get => this.orizzonteTemporale_sec; }
         public bool InterpolationActive { get => this.interpolationActive; }
         public float InterpolationThreshold_mt { get => this.interpolationThreshold_mt; }

src/backend/VVFGeoFleet/Web.config
@@ -31,6 +31,8 @@
     <add name="db" connectionString="mongodb://localhost:27017/vvfgeofleet" />
   </connectionStrings>
   <appSettings>
+    <add key="dbname" value="vvfgeofleet" />
+
     <add key="orizzonteTemporale_sec" value="86400" />
 
     <!-- interpolation configuration parameters - start -->
@@ -105,20 +107,18 @@
       </dependentAssembly>
       <dependentAssembly>
         <assemblyIdentity name="Microsoft.Owin.Security" publicKeyToken="31bf3856ad364e35" culture="neutral" />
-        <bindingRedirect oldVersion="0.0.0.0-4.0.0.0" newVersion="4.0.0.0" />
+        <bindingRedirect oldVersion="0.0.0.0-4.0.1.0" newVersion="4.0.1.0" />
       </dependentAssembly>
       <dependentAssembly>
         <assemblyIdentity name="Microsoft.Owin" publicKeyToken="31bf3856ad364e35" culture="neutral" />
+        <bindingRedirect oldVersion="0.0.0.0-4.0.1.0" newVersion="4.0.1.0" />
+      </dependentAssembly>
+      <dependentAssembly>
+        <assemblyIdentity name="System.Runtime.InteropServices.RuntimeInformation" publicKeyToken="b03f5f7f11d50a3a" culture="neutral" />
         <bindingRedirect oldVersion="0.0.0.0-4.0.0.0" newVersion="4.0.0.0" />
       </dependentAssembly>
     </assemblyBinding>
   </runtime>
-  <system.codedom>
-    <compilers>
-      <compiler language="c#;cs;csharp" extension=".cs" type="Microsoft.CodeDom.Providers.DotNetCompilerPlatform.CSharpCodeProvider, Microsoft.CodeDom.Providers.DotNetCompilerPlatform, Version=1.0.7.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" warningLevel="4" compilerOptions="/langversion:default /nowarn:1659;1699;1701" />
-      <compiler language="vb;vbs;visualbasic;vbscript" extension=".vb" type="Microsoft.CodeDom.Providers.DotNetCompilerPlatform.VBCodeProvider, Microsoft.CodeDom.Providers.DotNetCompilerPlatform, Version=1.0.7.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" warningLevel="4" compilerOptions="/langversion:default /nowarn:41008 /define:_MYTYPE=\&quot;Web\&quot; /optionInfer+" />
-    </compilers>
-  </system.codedom>
   <log4net>
     <appender name="LogFileAppender" type="log4net.Appender.RollingFileAppender">
       <param name="File" value="log.txt" />
@@ -144,4 +144,10 @@
       <appender-ref ref="TraceAppender" />
     </root>
   </log4net>
+  <system.codedom>
+    <compilers>
+      <compiler language="c#;cs;csharp" extension=".cs" type="Microsoft.CodeDom.Providers.DotNetCompilerPlatform.CSharpCodeProvider, Microsoft.CodeDom.Providers.DotNetCompilerPlatform, Version=2.0.1.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" warningLevel="4" compilerOptions="/langversion:default /nowarn:1659;1699;1701" />
+      <compiler language="vb;vbs;visualbasic;vbscript" extension=".vb" type="Microsoft.CodeDom.Providers.DotNetCompilerPlatform.VBCodeProvider, Microsoft.CodeDom.Providers.DotNetCompilerPlatform, Version=2.0.1.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" warningLevel="4" compilerOptions="/langversion:default /nowarn:41008 /define:_MYTYPE=\&quot;Web\&quot; /optionInfer+" />
+    </compilers>
+  </system.codedom>
 </configuration>

commit 33e7d8d22ffef244b4bf2646f908bb9234810cac

Database queries are logged again
This commit fixes a problem introduced by the commit d6e67bc808d5b232a8fed83240a7bddd79766e55. The queries are now logged again.

src/backend/Persistence.MongoDB/DbContext.cs
@@ -49,7 +49,18 @@ namespace Persistence.MongoDB
 
                 this.MapClasses();
 
-                var client = new MongoClient(connectionString);
+                var url = new MongoUrl(connectionString);
+                var settings = MongoClientSettings.FromUrl(url);
+                settings.ClusterConfigurator = cb =>
+                {
+                    cb.Subscribe<CommandStartedEvent>(e =>
+                    {
+                        if (!nonLoggedCommandNames.Contains(e.CommandName))
+                            log.Debug($"{e.CommandName} - {e.Command.ToJson()}");
+                    });
+                };
+
+                var client = new MongoClient(settings);
                 database = client.GetDatabase(databaseName);
 
                 this.CreateIndexes();